bcbbddea6b828a47dd2e96af177c8928d633051a
Ridham sharma <122754863+sharma4n@users.noreply.github.com>
2025-09-04 22:38:43 -0400

    Update run_all_scripts.R

---
+++ DGEA_2025-09-01/scripts/run_all_scripts.R
@@ -0,0 +1,7 @@
+# Master Script to run the entire project
+source("scripts/01_install_packages.R")
+source("scripts/02_data_loading.R")
+source("scripts/03_differential_expression.R")
+source("scripts/04_vizualization.R")
+
+print("All scripts executed successfully! Differential expression analysis complete.")
