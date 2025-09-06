5f2cb15e657373749421e7ef5c33a1b1893f0e4a
Ridham sharma <122754863+sharma4n@users.noreply.github.com>
2025-09-06 18:21:50 -0400

    Delete DGEA_2025_09_06 directory

--- DGEA_2025_09_06/scripts/run_all.R
+++
@@ -1,7 +0,0 @@
-# Master Script to run the entire Pasilla analysis
-source("scripts/01_install_packages.R")
-source("scripts/02_data_loading.R")
-source("scripts/03_differential_expression.R")
-source("scripts/04_visualization.R")
-
-print("All scripts executed successfully! Pasilla knockdown analysis complete.")
